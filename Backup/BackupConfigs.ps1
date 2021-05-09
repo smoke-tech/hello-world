05413b717bffb03b8632aa7e7eb650ed8a639828
stig-brahe <40003335+stig-brahe@users.noreply.github.com>
2021-05-09 23:23:00 +0200

    second draft

--- Backup/BackupConfigs.ps1
+++ Backup/BackupConfigs.ps1
@@ -1,18 +1,10 @@
-$servers = @("SAS00572","SAS00721","SAS00781","SAS00782","SAS00783","SAS00784","SAS00791","SAS00792","SPSPVBIGW-APP101","SPSVCRM-APP102","SPSVIFR-WEB101","SPSVNAV-APP101","SPSVTM-WEB101","SPSVWF-WEB102","SWB00542","SWB00771","TAPFON122","TAPFON124","SPSVFON-APP201")
+#$servers = @("SAS00572","SAS00721","SAS00781","SAS00782","SAS00783","SAS00784","SAS00791","SAS00792","SPSPVBIGW-APP101","SPSVCRM-APP102","SPSVIFR-WEB101","SPSVNAV-APP101","SPSVTM-WEB101","SPSVWF-WEB102","SWB00542","SWB00771","TAPFON122","TAPFON124","SPSVFON-APP201")
 
-#SWB00210
-
- 
+$servers = Get-Content -Path "c:\temp\servers.csv" 
+$servers
 
 $destpath = "C:\Install\AppDynClients"
-
- 
-
-$destFolder = $destpath + '\' + $servers
-
-$destFolder
-
- 
+$destpath
 
 foreach ($server in $servers)
 
@@ -23,33 +15,38 @@ foreach ($server in $servers)
   $sourcepath = "\\" + $server + "\c$\ProgramData\AppDynamics\DotNetAgent\Config"
 
   $sourcepath
-
+}
  
+foreach ($server in $servers)
+
+{
+
+  write-host $server
 
   if(!(Test-Path -Path $destpath\$server))
 
    {
 
-   New-Item -ItemType direcory -Path $destpath\$server
+   New-Item -ItemType directory -Path $destpath\$server
 
    Write-Host "Folder path has been created successfully at: "   $destpath\$server
 
+   } 
+   else{
+
+    Write-Host "The given folder path $destpath\$server already exists";
+    
+    }
  }
 
-else
+ foreach ($server in $servers)
 
-{
-
-Write-Host "The given folder path $destpath\$server already exists";
-
-}
-
+ {
  
-
-  Copy-Item $sourcepath\*.* $destpath\$server -Force
-
+   write-host "files copied from:" $server
  
+   Copy-Item $sourcepath\*.* $destpath\$server -Force
+ }
 
-  #\\sas00572\c$\ProgramData\AppDynamics\DotNetAgent\Config
 
-}
+
